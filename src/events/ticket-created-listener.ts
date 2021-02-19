edc2e2f12792cfb06a35f651d796e0dc47fd7468
Si Thu Win <sthuwin@gmail.com>
2021-02-19 22:52:43 +0800

    Bump Version

--- src/events/ticket-created-listener.ts
+++ src/events/ticket-created-listener.ts
@@ -1,7 +1,7 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
 import { Subjects } from "./subjects";
-import { TicketCreatedEvent } from "./ticket-created-event";
+import { TicketCreatedEvent } from "./model/ticket-created-event";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
